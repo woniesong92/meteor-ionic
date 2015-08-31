26becec293484bbe2f20146f7440e23adb8f06af
Jenny Kim <yk494@cornell.edu>
2015-08-31 00:04:31 -0400

    deleted ionkeyboard functions

--- components/ionKeyboard/ionKeyboard.js
+++ components/ionKeyboard/ionKeyboard.js
@@ -1,83 +1,83 @@
-Meteor.startup(function () {
-  if (Meteor.isCordova) {
-    IonKeyboard.disableScroll();
-  }
-});
+// Meteor.startup(function () {
+//   if (Meteor.isCordova) {
+//     IonKeyboard.disableScroll();
+//   }
+// });
 
-IonKeyboard = {
-  close: function () {
-    if (Meteor.isCordova) {
-      cordova.plugins.Keyboard.close();
-    }
-  },
+// IonKeyboard = {
+//   close: function () {
+//     if (Meteor.isCordova) {
+//       cordova.plugins.Keyboard.close();
+//     }
+//   },
 
-  show: function () {
-    if (Meteor.isCordova) {
-      cordova.plugins.Keyboard.show();
-    }
-  },
+//   show: function () {
+//     if (Meteor.isCordova) {
+//       cordova.plugins.Keyboard.show();
+//     }
+//   },
 
-  hideKeyboardAccessoryBar: function () {
-    if (Meteor.isCordova) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-  },
+//   hideKeyboardAccessoryBar: function () {
+//     if (Meteor.isCordova) {
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+//     }
+//   },
 
-  showKeyboardAccessoryBar: function () {
-    if (Meteor.isCordova) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
-    }
-  },
+//   showKeyboardAccessoryBar: function () {
+//     if (Meteor.isCordova) {
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+//     }
+//   },
 
-  disableScroll: function () {
-    if (Meteor.isCordova) {
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-  },
+//   disableScroll: function () {
+//     if (Meteor.isCordova) {
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//   },
 
-  enableScroll: function () {
-    if (Meteor.isCordova) {
-      cordova.plugins.Keyboard.disableScroll(false);
-    }
-  }
-};
+//   enableScroll: function () {
+//     if (Meteor.isCordova) {
+//       cordova.plugins.Keyboard.disableScroll(false);
+//     }
+//   }
+// };
 
-window.addEventListener('native.keyboardshow', function (event) {
-  // TODO: Android is having problems
-  if (Platform.isAndroid()) {
-    return;
-  }
+// window.addEventListener('native.keyboardshow', function (event) {
+//   // TODO: Android is having problems
+//   if (Platform.isAndroid()) {
+//     return;
+//   }
 
-  $('body').addClass('keyboard-open');
-  var keyboardHeight = event.keyboardHeight;
-  // event.stopPropagation();
+//   $('body').addClass('keyboard-open');
+//   var keyboardHeight = event.keyboardHeight;
+//   // event.stopPropagation();
 
-  $('[data-keyboard-attach]').each(function (index, el) {
-    $(el).velocity({
-      bottom: keyboardHeight
-    }, {
-      duration: 170
-    });
+//   $('[data-keyboard-attach]').each(function (index, el) {
+//     $(el).velocity({
+//       bottom: keyboardHeight
+//     }, {
+//       duration: 170
+//     });
 
-    $('.content').css('padding-bottom', keyboardHeight);
-  });
-});
+//     $('.content').css('padding-bottom', keyboardHeight);
+//   });
+// });
 
-window.addEventListener('native.keyboardhide', function (event) {
-  // TODO: Android is having problems
-  if (Platform.isAndroid()) {
-    return;
-  }
+// window.addEventListener('native.keyboardhide', function (event) {
+//   // TODO: Android is having problems
+//   if (Platform.isAndroid()) {
+//     return;
+//   }
 
-  $('body').removeClass('keyboard-open');
+//   $('body').removeClass('keyboard-open');
 
-  $('[data-keyboard-attach]').each(function (index, el) {
-    $(el).velocity({
-      bottom: 0
-    }, {
-      duration: 0
-    });
+//   $('[data-keyboard-attach]').each(function (index, el) {
+//     $(el).velocity({
+//       bottom: 0
+//     }, {
+//       duration: 0
+//     });
 
-    $('.content').css('padding-bottom', '0px');
-  });
-});
+//     $('.content').css('padding-bottom', '0px');
+//   });
+// });
